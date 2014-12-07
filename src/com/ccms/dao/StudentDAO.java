431253c873854ecaff19073c734ebab2aaa20df2
geekymv <ym2011678@foxmail.com>
2014-12-07 15:36:32 +0800

    添加项目所需要的包

---
+++ src/com/ccms/dao/StudentDAO.java
@@ -0,0 +1,14 @@
+package com.ccms.dao;
+
+import com.ccms.pojo.Student;
+
+public interface StudentDAO {
+	
+	/**
+	 * 保存
+	 * @param student
+	 * @return
+	 */
+	public int save(Student student);
+
+}
---
+++ src/com/ccms/pojo/Student.java
@@ -0,0 +1,5 @@
+package com.ccms.pojo;
+
+public class Student {
+
+}
